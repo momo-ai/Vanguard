c45c0196c6253b49e3454fde0e234ea5022b95f3
ben <ben@veridise.com>
2022-04-02 14:41:22 -0500

    Pushing suicide analysis stuff

---
+++ lib/detectors/SuicideDetector/SuicideDetector.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Jon Stephens on 3/19/22.
+//
+
+#include "SuicideDetector.h"
+#include "SuicideAnalysis.h"
+
+namespace Suicide {
+    SuicideDetector::SuicideDetector() : IntraproceduralVanguard(ID) {
+        const blockchain::Blockchain *chain = blockchain();
+        analyzer = new SuicideAnalysis(chain);
+        Vanguard::registerAnalysis(analyzer);
+    }
+
+    SuicideDetector::~SuicideDetector() {
+        delete analyzer;
+    }
+
+    char SuicideDetector::ID = 0;
+    static RegisterPass<SuicideDetector> X("suicide", "Basic Suicide Detector",
+                                              true /* Only looks at CFG */,
+                                              true /* Analysis Pass */);
+
+}
---
+++ lib/detectors/SuicideDetector/SuicideDetector.h
@@ -0,0 +1,26 @@
+//
+// Created by Jon Stephens on 3/19/22.
+//
+
+#ifndef VANGUARD_ReentrancyEXAMPLE_H
+#define VANGUARD_ReentrancyEXAMPLE_H
+
+#include "../../IntraproceduralVanguard.h"
+#include "SuicideAnalysis.h"
+
+using namespace vanguard;
+
+namespace Suicide {
+    class SuicideDetector : public IntraproceduralVanguard {
+    public:
+        static char ID;
+        SuicideDetector();
+        ~SuicideDetector();
+    private:
+        SuicideAnalysis *analyzer;
+
+    };
+}
+
+
+#endif //VANGUARD_Suicide_H
