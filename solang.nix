32271abc0098b26009f7f9991daee3ac37eed938
momo-ai <62407942+momo-ai@users.noreply.github.com>
2022-09-09 11:24:05 -0500

    add x86-linux version for solang.nix

--- solang.nix
+++ solang.nix
@@ -9,6 +9,14 @@ let
         url = "https://github.com/hyperledger-labs/solang/releases/download/v0.1.12/solang-mac-arm";
         sha256 = "0f5r9666kbz9nf67h00c5my2gs51a8znzp1mcvykqp4x6qp5vgp7";
       };
+      x86_64-darwin = {
+        url = "https://github.com/hyperledger-labs/solang/releases/download/v0.1.12/solang-mac-intel";
+        sha256 = "177h7fk73ky1952dsns57byvhxmx0cc77xhrkdcg08j6y4yh6lww";
+      };
+      x86_64-linux = {
+        url = "https://github.com/hyperledger/solang/releases/download/v0.1.12/solang-linux-x86-64";
+        sha256 = "sha256-Mx7PjmzMU98gDH0fOxANpTgAj5nOT5mbTyT2ixcV068=";
+      }; 
     };
   };
 
