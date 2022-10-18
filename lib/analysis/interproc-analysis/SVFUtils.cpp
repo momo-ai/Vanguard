f55ece8ec641062caffc0ce255d6d72a7c11f226
Kostas Ferles <kferles@gmail.com>
2022-10-18 11:19:21 -0500

    Move SVFUtils definitions to cpp file.

--- lib/analysis/interproc-analysis/SVFUtils.cpp
+++ lib/analysis/interproc-analysis/SVFUtils.cpp
@@ -4,14 +4,134 @@
 
 #include "SVFUtils.h"
 
-SVF::SVFModule * analysis::SVFUtils::svfModule = nullptr;
+namespace analysis {
+    SVF::SVFModule *SVFUtils::svfModule = nullptr;
 
-SVF::SVFIR * analysis::SVFUtils::pag = nullptr;
+    SVF::SVFIR *SVFUtils::pag = nullptr;
 
-SVF::BVDataPTAImpl * analysis::SVFUtils::pa = nullptr;
+    SVF::BVDataPTAImpl *SVFUtils::pa = nullptr;
 
-SVF::PTACallGraph * analysis::SVFUtils::callgraph = nullptr;
+    SVF::PTACallGraph *SVFUtils::callgraph = nullptr;
 
-SVF::SVFG * analysis::SVFUtils::svfg = nullptr;
+    SVF::SVFG *SVFUtils::svfg = nullptr;
 
-std::map<const llvm::Value*, std::set<const SVF::VFGNode*>> analysis::SVFUtils::cachedDeps;
+    std::map<const llvm::Value *, std::set<const SVF::VFGNode *>> SVFUtils::cachedDeps;
+
+    bool SVFUtils::dependsOnTrg(const llvm::Value *src, const llvm::Value *trg) {
+        if (cachedDeps.find(src) == cachedDeps.end()) {
+            std::set<const SVF::VFGNode *> deps;
+            return traverseMemDependencies(src, deps, trg);
+        } else {
+            auto deps = cachedDeps[src];
+            return std::any_of(deps.begin(), deps.end(), [&trg](auto node) { return node->getValue() == trg; });
+        }
+    }
+
+    std::vector<const llvm::Value*> SVFUtils::valueMemDeps(const llvm::Value *src) {
+        if (cachedDeps.find(src) == cachedDeps.end()) {
+            std::set<const SVF::VFGNode*> deps;
+            traverseMemDependencies(src, deps);
+            cachedDeps[src] = deps;
+        }
+
+        // Convert VFGNode -> llvm::Value
+        std::vector<const llvm::Value*> valDeps;
+        std::set<const SVF::VFGNode*> &memDeps = cachedDeps[src];
+        std::transform(memDeps.begin(), memDeps.end(), std::back_inserter(valDeps),
+                       [](auto mDep) { return mDep->getValue();});
+
+        // Filter out null values
+        std::vector<const llvm::Value*> nonNullValDeps;
+        std::copy_if(valDeps.begin(), valDeps.end(), std::back_inserter(nonNullValDeps),
+                     [](auto val) { return val != nullptr;});
+        return std::move(nonNullValDeps);
+    }
+
+    void SVFUtils::initSVFModule() {
+        if (svfModule == nullptr) {
+            svfModule = SVF::LLVMModuleSet::getLLVMModuleSet()->getSVFModule();
+        }
+    }
+
+    void SVFUtils::initPAG() {
+        if (pag == nullptr) {
+            initSVFModule();
+            SVF::SVFIRBuilder builder;
+            pag = builder.build(svfModule);
+        }
+    }
+
+    void SVFUtils::initPointerAnalysis() {
+        if (pa == nullptr) {
+            initPAG();
+            // TODO: make the following configurable
+            pa = SVF::AndersenWaveDiff::createAndersenWaveDiff(pag);
+        }
+    }
+
+    void SVFUtils::initCallGraph() {
+        if (callgraph == nullptr) {
+            initPointerAnalysis();
+            callgraph = pa->getPTACallGraph();
+        }
+    }
+
+    void SVFUtils::initSVFG() {
+        if (svfg == nullptr) {
+            // Ensure pa is initialized.
+            initPointerAnalysis();
+            SVF::SVFGBuilder svfBuilder;
+            svfg = svfBuilder.buildFullSVFG(pa);
+        }
+    }
+
+    bool SVFUtils::traverseMemDependencies(const llvm::Value *src, std::set<const SVF::VFGNode*> &deps, const llvm::Value *trg)
+    {
+        initSVFG();
+
+        SVF::PAGNode* pNode = pag->getGNode(pag->getValueNode(src));
+        const SVF::VFGNode* vNode = svfg->getDefVFGNode(pNode);
+        SVF::FIFOWorkList<const SVF::VFGNode*> worklist;
+        worklist.push(vNode);
+
+        /// Traverse along SVFG
+        while (!worklist.empty())
+        {
+            vNode = worklist.pop();
+
+            if (trg != nullptr) {
+                if (trg == vNode->getValue()) {
+                    return true;
+                }
+            }
+
+            // Chase uses
+            for (auto it = vNode->OutEdgeBegin(), eit =
+                    vNode->OutEdgeEnd(); it != eit; ++it)
+            {
+                SVF::SVFGEdge* edge = *it;
+                SVF::SVFGNode* succNode = edge->getDstNode();
+                if (deps.find(succNode) == deps.end())
+                {
+                    deps.insert(succNode);
+                    worklist.push(succNode);
+                }
+            }
+
+            // Chase defs
+            for (auto it = vNode->InEdgeBegin(), eit =
+                    vNode->InEdgeEnd(); it != eit; ++it)
+            {
+                SVF::VFGEdge* edge = *it;
+                SVF::VFGNode* predNode = edge->getSrcNode();
+                if (deps.find(predNode) == deps.end())
+                {
+                    deps.insert(predNode);
+                    worklist.push(predNode);
+                }
+            }
+        }
+
+        return false;
+    }
+}
